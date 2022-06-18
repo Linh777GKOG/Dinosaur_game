e83675a257f21d08ad7c0d31b12f3f4075650ec1
Linh Vu <linh777gkog@gmail.com>
2022-06-18 22:29:18 +0700

    set pixel

--- script.js
+++ script.js
@@ -77,3 +77,15 @@ function handleLose() {
     startScreenElem.classList.remove('hide');
   }, 100);
 }
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+  }
+
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
