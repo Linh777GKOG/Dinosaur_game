ff2dea562379ae844e77476947e6768068b71099
Linh Vu <linh777gkog@gmail.com>
2022-06-18 22:28:38 +0700

    check point

---
+++ script.js
@@ -0,0 +1,37 @@
+import { updateGround, setupGround } from './ground.js';
+import { updateDino, setupDino, getDinoRect, setDinoLose } from './dino.js';
+import { updateCactus, setupCactus, getCactusRects } from './cactus.js';
+
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
+
+const worldElem = document.querySelector('[data-world]');
+const scoreElem = document.querySelector('[data-score]');
+const startScreenElem = document.querySelector('[data-start-screen]');
+
+setPixelToWorldScale();
+window.addEventListener('resize', setPixelToWorldScale);
+document.addEventListener('keydown', handleStart, { once: true });
+
+let lastTime;
+let speedScale;
+let score;
+function update(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+  const delta = time - lastTime;
+
+  updateGround(delta, speedScale);
+  updateDino(delta, speedScale);
+  updateCactus(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+  if (checkLose()) return handleLose();
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
